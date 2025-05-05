6ff226a4f4f9ca11d139912eeb6f02cf15513170
keiraIT <kmmcgi2@gmail.com>
2025-05-05 00:26:00 -0500

    motor_test.py

--- src/motor_test.py
+++ src/motor_test.py
@@ -1,3 +1,4 @@
+##This is a simple motor test, uses command protocol foor Elegoo SMart Car to move and is bare minimum movement code
 import io
 import numpy as np
 import socket
@@ -7,7 +8,7 @@ HOST = "192.168.4.1"  # The server's hostname or IP address
 PORT = 100
  
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect( (HOST, PORT) )
     print("connecting to ESP32-S3-Eye")
     #give a two second heads up for capturing the next image
     time.sleep(2)
